439bec5d5e8f1f4c023b610b4e628b49baed3e00
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-20 00:04:04 +0100

    [UPD] seventh ex don

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -40,7 +40,14 @@ console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+// const lewisIndex = null;
+for (let i = 0; i <= teachers.length; i++) {
+  if (teachers[i] === 'Lewis') {
+         const lewisIndex = teachers[i]
+        console.log(lewisIndex)
+  }
+  
+}
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
