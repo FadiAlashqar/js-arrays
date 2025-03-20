dcf6321ad0af868d1c13d2f00534a91e8f8093c7
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-20 01:29:10 +0100

    [UPD] bonus 4 done

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -31,7 +31,15 @@ console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+// const isFabioPresent = null;
+for (let i = 0; i < teachers.length; i++) {
+    if (teachers[i] === 'Fabio') {
+      const isFabioPresent = `${teachers[i]} Presente`;
+      console.log(isFabioPresent)
+    }
+  
+}
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
