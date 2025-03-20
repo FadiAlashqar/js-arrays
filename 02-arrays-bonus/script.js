b5d1fed3f170c97bea008d51dc95b02b397ce400
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-20 11:07:20 +0100

    [UPD] bonus 5 corrected

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -42,5 +42,10 @@ for (let i = 0; i < teachers.length; i++) {
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = teachers.join(", ");
+let string = ''
+for (let i = 0; i < teachers.length; i++) {
+    string = string + teachers[i] + ", ";
+}
+
+const teachersString = string;
 console.log(teachersString)
