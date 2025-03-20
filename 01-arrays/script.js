24f24ae5dcca12f25a84a85c693efd60cdfa0e2c
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-20 09:45:08 +0100

    [UPD] ex 3 corrected

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -19,9 +19,7 @@ console.log(teachers);
 
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = teachers[6];
-teachers.pop();
-console.log(teachers);
+const lastTeacher = teachers.pop();
 console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
